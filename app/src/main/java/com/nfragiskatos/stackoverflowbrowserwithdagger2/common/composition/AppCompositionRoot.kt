8431aaf77afa11576798cfc04b72db021b273d80
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-03 10:11:35 -0700

    Lazy initialized the global services in AppCompositionRoot.kt (Retrofit & StackOverflowApi). Generally a good idea to prevent poor performance on app startup if you have lots of services, and some services take a considerable time to initialize.

--- app/src/main/java/com/nfragiskatos/stackoverflowbrowserwithdagger2/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/nfragiskatos/stackoverflowbrowserwithdagger2/common/composition/AppCompositionRoot.kt
@@ -1,20 +1,24 @@
 package com.nfragiskatos.stackoverflowbrowserwithdagger2.common.composition
 
 import FetchQuestionDetailsUseCase
+import androidx.annotation.UiThread
 import com.nfragiskatos.stackoverflowbrowserwithdagger2.Constants
 import com.nfragiskatos.stackoverflowbrowserwithdagger2.networking.StackoverflowApi
 import com.nfragiskatos.stackoverflowbrowserwithdagger2.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
 
-    private val stackOverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackOverflowApi: StackoverflowApi by lazy { retrofit.create(StackoverflowApi::class.java) }
 
     val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackOverflowApi)
 
