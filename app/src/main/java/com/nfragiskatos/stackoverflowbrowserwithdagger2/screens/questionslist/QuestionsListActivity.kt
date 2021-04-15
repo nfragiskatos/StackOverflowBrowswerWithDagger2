ecb6cd521a899e025d0bfd2c6b1d064c22c15af9
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-15 08:33:25 -0700

    - Removed unneeded injection.

--- app/src/main/java/com/nfragiskatos/stackoverflowbrowserwithdagger2/screens/questionslist/QuestionsListActivity.kt
+++ app/src/main/java/com/nfragiskatos/stackoverflowbrowserwithdagger2/screens/questionslist/QuestionsListActivity.kt
@@ -1,22 +1,13 @@
 package com.nfragiskatos.stackoverflowbrowserwithdagger2.screens.questionslist
 
 import android.os.Bundle
-import android.util.Log
 import com.nfragiskatos.stackoverflowbrowserwithdagger2.R
-import com.nfragiskatos.stackoverflowbrowserwithdagger2.screens.common.ScreensNavigatorImpl
 import com.nfragiskatos.stackoverflowbrowserwithdagger2.screens.common.activities.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class QuestionsListActivity : BaseActivity() {
-
-
-    @Inject
-    lateinit var screensNavigator: ScreensNavigatorImpl
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.e("QuestionsListActivity", "$screensNavigator")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_frame)
 
